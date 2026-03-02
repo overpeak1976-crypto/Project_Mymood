edc10747df10e06445fa9234b90c9e5bb0743a91
overpeak1976-crypto <overpeak1976@gmail.com>
2026-03-03 03:15:22 +0700

    update

---
+++ mymood/frontend/app/index.tsx
@@ -0,0 +1,10 @@
+import "./global.css"
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-lg font-bold">mymood</Text>
+    </View>
+  );
+}
